5e40f34f753edbdc2dd7e2df20f2de8c09e0c015
abdulsalamabdul1415@gmail.com <abdulsalamabdul1415@gmail.com>
2024-06-07 15:14:22 +0530

    Git Ignore File Added

--- BLOG_API/DomainEntity/obj/Debug/net8.0/AppDomain.AssemblyInfo.cs
+++ BLOG_API/DomainEntity/obj/Debug/net8.0/AppDomain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b33838213860d8bfd3c1ab889f49fdb29210d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
